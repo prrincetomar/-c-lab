194a28c8406e3fd5d629312d6b1767d3be1ad19d
Prince Tomar <151778642+prrincetomar@users.noreply.github.com>
2024-11-25 19:58:25 +0530

    Update strcat-without-string.c

--- strcat-without-string.c
+++ strcat-without-string.c
@@ -11,20 +11,16 @@ void main ()
     printf("string s2:");
     gets(s2);
 
-    for( i = 0; s1[i] !='\0'; i++)
-    {}
+    for( i = 0; s1[i] !='\0'; i++){
+    }
 
-    while (s2[j] != '\0')
-    {   s1[i]= s2[j];
+    while (s2[j] != '\0'){
+       s1[i]= s2[j];
         i++;
         j++;
     }
 
-s1[i]='\0';
-puts(s1);
-
-
-
-
+    s1[i]='\0';
+    puts(s1);
 
 }
